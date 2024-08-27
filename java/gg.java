776bda52cc553b3f359922847c0715d572e2fdee
amulya8522 <amulyagangadi@gmail.com>
2024-08-27 12:38:45 +0530

    Add files via upload

---
+++ java/gg.java
@@ -0,0 +1,8 @@
+class main{
+    public static void main(String[] args){
+        int first=20;
+        int secnd=30;
+        int sum=first+secnd;
+        System.out.println(first+"+"+ secnd +"="+sum);    
+    }
+}
